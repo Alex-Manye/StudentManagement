86c654d53bf7648ca961b0192f11c17b6eeb5ec4
joan_m <joanmiquelortega@gmail.com>
2020-03-08 16:58:26 +0100

    Add method CreateStudent

--- CodeFirstEntityFramework/OperationSQL.cs
+++ CodeFirstEntityFramework/OperationSQL.cs
@@ -15,8 +15,20 @@ namespace CodeFirstEntityFramework
                 students.ForEach(student => Console.WriteLine(student.StudentName));
             }
         }
+        public static void CreateStudent(string Name, string LastName, DateTime Birthday)
+        {
+            StudentContext db = new StudentContext();
+            StudentDao student = new StudentDao
+            {
+                StudentName = Name,
+                StudentLastName = LastName,
+                BirthDate = Birthday
+            };
+            db.Students.Add(student);
+            db.SaveChangesAsync();
+            Console.WriteLine("Create Successfully");
+        }
 
-       
 
     }
 }
