7c60616fcf029fca5ffc86d0181b9bb72662340a
raimonestanyol <61312986+raimonestanyol@users.noreply.github.com>
2020-03-08 19:33:39 +0100

    Installed Log4Net

--- DAO CRUD/DAO CRUD/Class1.cs
+++ DAO CRUD/DAO CRUD/Class1.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using log4net;
 
 namespace DAO_CRUD
 {
     public class Class1
     {
-
+        SqlConnection connection;
+        SqlCommand command;
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Class1));
     }
 }
