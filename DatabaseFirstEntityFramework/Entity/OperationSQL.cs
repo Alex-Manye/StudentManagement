e0d018951206cb0670e4594903615d2e3fe71052
joan_m <joanmiquelortega@gmail.com>
2020-03-08 14:11:11 +0100

    Add ReadStudents

---
+++ DatabaseFirstEntityFramework/Entity/OperationSQL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace DatabaseFirstEntityFramework.Entity
+{
+    public class OperationSql
+    {
+        public static void ReadStudents()
+        {
+            using(StudentEntities db = new StudentEntities())
+            {
+                var students = (from student in db.Student
+                                select student).ToList();
+                students.ForEach(student => Console.WriteLine(student.StudentName));
+            }
+        }
+
+        public static void UpdateStudents()
+        {
+            using (StudentEntities db = new StudentEntities())
+            {
+                object p = db.Student.Where(students => students.StudentId == 1).ToList().SetValue(student => student.S = 1000);
+            }
+            }
+    }
+}
