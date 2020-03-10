73f6b39ffcfd3ab066b0e9021f855f996f530294
joan_m <joanmiquelortega@gmail.com>
2020-03-10 14:08:18 +0100

    Add interface SqlServer

---
+++ Code_and_Data_First_Entity/DataFirstEntityFirst/SqlServer.cs
@@ -0,0 +1,12 @@
+﻿using DataFirstEntityFirst.Entity;
+
+namespace DataFirstEntityFirst
+{
+    interface SqlServer
+    {
+        void ReadStudents();
+        void CreateStudent(Student student);
+        void UpdateStudent(int id, string Name);
+        void DeleteStudent(int id);
+    }
+}
