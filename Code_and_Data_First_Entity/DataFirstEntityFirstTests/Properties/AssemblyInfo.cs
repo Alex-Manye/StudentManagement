a348cc09b06c9568ca73667a8198964d859ea5bd
joan_m <joanmiquelortega@gmail.com>
2020-03-10 14:08:35 +0100

    other changes

--- Code_and_Data_First_Entity/DataFirstEntityFirstTests/Properties/AssemblyInfo.cs
+++ Code_and_Data_First_Entity/DataFirstEntityFirstTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
@@ -33,4 +34,3 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
